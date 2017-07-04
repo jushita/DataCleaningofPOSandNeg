fcfab232d8e543a8eb8a1f6d781a2e463aac6b48
Jushita Rahman <jushitaa@gmail.com>
2017-07-04 14:14:30 -0400

    likelihood added

--- ManipulateData.py
+++ ManipulateData.py
@@ -56,3 +56,36 @@ class ManipulateData():
         with open("combined_files.txt", "w") as file:
             file.writelines(data1)
             file.writelines(data2)
+
+    def matchLikelihoodRatio(self, _file):
+        new_file = open("matched_likelihood_ratios.txt", "w")
+
+        #read lines of file to list
+        with open(_file, "r") as file:
+            for i, line in enumerate(file):
+                line = line.rstrip("\n")
+                split_line = line.split('\t')
+
+                file_protein1 = "L_PIP/" + split_line[0] + ".txt"
+                file_protein2 = "L_PIP/" + split_line[1] + ".txt"
+
+                if os.path.isfile(file_protein1):
+                    with open(file_protein1) as file:
+                        for line in file:
+                            other_protein = line.split("\t")[0]
+                            if split_line[1] == other_protein:
+                                lpip = line.split('\t')[5]
+                                split_line.append(lpip.rstrip('\n'))
+                elif os.path.isfile(file_protein2):
+                    with open(file_protein2) as file:
+                        for line in file:
+                            other_protein = line.split("\t")[0]
+                            if split_line[0] == other_protein:
+                                lpip = line.split('\t')[5]
+                                split_line.append(lpip.rstrip('\n'))
+                else:
+                    split_line.append("Null")
+
+                new_file.write("\t".join(split_line) + "\n")
+
+    print ("Done")
